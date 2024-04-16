66b5634bf7578b66cda1f93ad57177ef701b7b94
liaozhengyang <liaozhengyang@star-net.cn>
2024-04-16 15:07:50 +0800

    update:TextInput component docs

--- src/stories/TextInput.stories.tsx
+++ src/stories/TextInput.stories.tsx
@@ -1,7 +1,6 @@
-import { CloseButton, TextInput } from '@mantine/core';
+import { TextInput, rem } from '@mantine/core';
 import { ArgTypes, Meta, StoryObj } from '@storybook/react';
-import { IconAt, IconChevronDown } from '@tabler/icons-react';
-import { useState } from 'react';
+import { IconAt } from '@tabler/icons-react';
 
 const meta = {
 	title: 'Example/TextInput',
@@ -12,144 +11,164 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const OPTIONS_SIZE = ['xs', 'sm', 'md', 'lg', 'xl'];
+const SIZE = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+const INPUT_VARIANT = ['default', 'filled', 'unstyled'];
 
 /**
- * `Input` component is used as base for some other inputs (NativeSelect, TextInput, Textarea, etc.).
- * The purpose of the `Input` is to provide shared styles and features to other inputs.
+ * `TextInput` component supports [Input](/docs/example-input--docs) and
+ * [Input.Wrapper](/docs/example-input--docs) components features and all `input` element props.
+ * `TextInput` documentation does not include all features supported by the component – see Input documentation to learn about all available features.
  */
 export const Usage: Story = {
 	args: {
-		disabled: false,
-		error: undefined,
-		id: undefined,
-		size: 'sm',
-		placeholder: 'Input component',
+		variant: INPUT_VARIANT[0],
+		size: SIZE[1],
+		radius: SIZE[1],
+		label: 'Input label',
+		withAsterisk: false,
+		description: 'Input description',
+		error: '',
 	},
 	argTypes: {
-		disabled: {
-			description: 'Sets `disabled` attribute on the input element',
-			type: 'boolean',
+		variant: {
+			control: 'select',
+			type: '"default"|"filled"|"unstyled"',
+			description: 'Input variant',
+			options: INPUT_VARIANT,
 			table: {
-				defaultValue: { summary: 'false' },
-			},
-		},
-		error: {
-			description:
-				'Determines whether the input should have error styles and `aria-invalid` attribute',
-			type: 'React.ReactNode',
-			control: 'text',
-			table: {
-				defaultValue: { summary: undefined },
-			},
-		},
-		id: {
-			description: 'Input element id',
-			type: 'string',
-			control: 'text',
-			table: {
-				defaultValue: { summary: undefined },
+				defaultValue: { summary: 'default' },
 			},
 		},
 		size: {
-			type: 'MantineSize | (string & {})',
 			control: 'select',
-			options: OPTIONS_SIZE,
+			type: 'MantineSize | (string & {})',
+			description:
+				"Controls input `height` and horizontal `padding`, `'sm'` by default",
+			options: SIZE,
 			table: {
 				defaultValue: { summary: 'sm' },
 			},
 		},
-		placeholder: {
-			type: 'string',
-			control: 'text',
+		radius: {
+			control: 'select',
+			type: 'MantineRadius | number',
+			description:
+				'Key of `theme.radius` or any valid CSS value to set `border-radius`, numbers are converted to rem, `theme.defaultRadius` by default',
+			options: SIZE,
 			table: {
-				defaultValue: { summary: 'Input component' },
+				defaultValue: { summary: 'sm' },
+			},
+		},
+		label: {
+			control: 'text',
+			type: 'React.ReactNode',
+			description:
+				'Contents of `Input.Label` component. If not set, label is not rendered.',
+			table: {
+				defaultValue: { summary: 'Input label' },
+			},
+		},
+		withAsterisk: {
+			control: 'boolean',
+			type: 'boolean	',
+			description:
+				'Determines whether the required asterisk should be displayed. Overrides `required` prop. Does not add required attribute to the input. `false` by default',
+			table: {
+				defaultValue: { summary: 'false' },
+			},
+		},
+		description: {
+			control: 'text',
+			type: 'React.ReactNode',
+			description:
+				'Contents of `Input.Description` component. If not set, description is not rendered.',
+			table: {
+				defaultValue: { summary: 'Input description' },
+			},
+		},
+		error: {
+			control: 'text',
+			type: 'React.ReactNode',
+			description:
+				'Contents of `Input.Error` component. If not set, error is not rendered.',
+			table: {
+				defaultValue: { summary: '' },
 			},
 		},
 	} as Partial<ArgTypes<{}>>,
 };
 
 /**
- * Input supports `leftSection` and `rightSection` props.
- * These sections are rendered with absolute position inside the input wrapper.
+ * `TextInput` supports `leftSection` and `rightSection` props. These sections are rendered with absolute position inside the input wrapper.
  * You can use them to display icons, input controls or any other elements.
- *
  * You can use the following props to control sections styles and content:
- * - `rightSection`/`leftSection` – React node to render on the corresponding side of input
  *
+ * - `rightSection`/`leftSection` – React node to render on the corresponding side of input
  * - `rightSectionWidth`/`leftSectionWidth` – controls width of the right section and padding on the corresponding side of the input.
  * By default, it is controlled by component `size` prop.
- *
  * - `rightSectionPointerEvents`/`leftSectionPointerEvents` – controls `pointer-events` property of the section.
  * If you want to render a non-interactive element, set it to `none` to pass clicks through to the input.
  */
 export const Left_and_right_sections: Story = {
 	...Usage,
-	render(props) {
-		const [value, setValue] = useState('Clear me');
+	render: (props) => {
+		const icon = <IconAt style={{ width: rem(16), height: rem(16) }} />;
 		return (
 			<>
 				<TextInput
 					{...props}
+					leftSectionPointerEvents='none'
+					leftSection={icon}
+					label='Your email'
 					placeholder='Your email'
-					leftSection={<IconAt size={16} />}
 				/>
 				<TextInput
 					{...props}
-					placeholder='Clearable input'
-					value={value}
-					onChange={(event) => setValue(event.currentTarget.value)}
-					rightSectionPointerEvents='all'
 					mt='md'
-					rightSection={
-						<CloseButton
-							aria-label='Clear input'
-							onClick={() => setValue('')}
-							style={{ display: value ? undefined : 'none' }}
-						/>
-					}
+					rightSectionPointerEvents='none'
+					rightSection={icon}
+					label='Your email'
+					placeholder='Your email'
 				/>
 			</>
 		);
 	},
 };
 
-/**
- * Input is a polymorphic component, the default root element is `input`, but it can be changed to any other element or component.
- *
- * Example of using react-imask with Input:
- *
- * ```js
- * import { Input } from '@mantine/core';
- * import { IMaskInput } from 'react-imask';
- *
- * function Demo() {
- *   return <Input component={IMaskInput} mask="+7 (000) 000-00-00" placeholder="Your phone" />;
- * }
- * ```
- *
- * Example of using Input as button and select:
- */
-export const Change_input_element: Story = {
+export const Error_state: Story = {
+	...Usage,
+	render: (props) => {
+		return (
+			<>
+				<TextInput
+					{...props}
+					label='Boolean error'
+					placeholder='Boolean error'
+					error
+				/>
+				<TextInput
+					{...props}
+					mt='md'
+					label='With error message'
+					placeholder='With error message'
+					error='Invalid name'
+				/>
+			</>
+		);
+	},
+};
+
+export const Disabled_state: Story = {
 	...Usage,
 	render(props) {
 		return (
-			<>
-				<TextInput {...props} component='button' pointer>
-					Button input
-				</TextInput>
-
-				<TextInput
-					{...props}
-					component='select'
-					rightSection={<IconChevronDown size={14} stroke={1.5} />}
-					pointer
-					mt='md'
-				>
-					<option value='1'>1</option>
-					<option value='2'>2</option>
-				</TextInput>
-			</>
+			<TextInput
+				{...props}
+				disabled
+				label='Disabled input'
+				placeholder='Disabled input'
+			/>
 		);
 	},
 };
